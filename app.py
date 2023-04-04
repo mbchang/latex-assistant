2d8832ebc260a43a1e935609acfd57fb43465534
Michael Chang <95changmichael13@gmail.com>
2023-04-04 11:09:01 -0700

    feat: render latex in backend

--- app.py
+++ app.py
@@ -1,35 +1,59 @@
+from flask import Flask, render_template, request, send_file
+import tempfile
 import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+import subprocess
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/")
 def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def render_tikz():
+    tikz_code = request.form.get("tikz_code", "")
+    with tempfile.NamedTemporaryFile(suffix=".tex") as tex_file:
+        print(f"Temporary file location: {tex_file.name}")
+        tex_file.write(tikz_code.encode())
+        tex_file.flush()
+        subprocess.call(
+            [
+                "pdflatex",
+                # f"-interaction=nonstopmode -output-directory={os.path.dirname(tex_file.name)}",
+                # -interaction=nonstopmode causes it to save the file in current directory
+                f"-output-directory={os.path.dirname(tex_file.name)}",
+                tex_file.name,
+            ]
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        subprocess.call(
+            [
+                "convert",
+                "-density",
+                "300",
+                "-quality",
+                "90",
+                "-flatten",
+                "-sharpen",
+                "0x1.0",
+                # "-resize",
+                # "800x800",
+                "-trim",
+                "-bordercolor",
+                "#FFFFFF",
+                # "-border",
+                # "1x1",
+                # "-alpha",
+                # "remove",
+                # "-background",
+                # "none",
+                f"{tex_file.name[:-4]}.pdf",
+                f"{tex_file.name[:-4]}.png",
+            ]
+        )
+    return send_file(f"{tex_file.name[:-4]}.png", mimetype="image/png")
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == "__main__":
+    app.run()
