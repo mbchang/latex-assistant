bbd7805a3dcf0817f1e1090e2e8503e467607366
Michael Chang <95changmichael13@gmail.com>
2023-04-04 12:57:32 -0700

    feat: send png as a base64-encoded string instead of saving it to the static folder and then reading the url

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, send_file, url_for
+import base64
+from flask import Flask, render_template, request, url_for
+import glob
 import tempfile
 import os
 import subprocess
@@ -42,8 +44,8 @@ def render_tikz():
                 "-trim",
                 "-bordercolor",
                 "#FFFFFF",
-                # "-border",
-                # "1x1",
+                "-border",
+                "1x1",
                 # "-alpha",
                 # "remove",
                 # "-background",
@@ -53,13 +55,23 @@ def render_tikz():
             ]
         )
         # a hacky method that copies the file in the tmp folder to the static folder
-        subprocess.call(["cp", f"{tex_file.name[:-4]}.png", "static/"])
+        # subprocess.call(["cp", f"{tex_file.name[:-4]}.png", "static/"])
     # this by itself just renders the entire image in the browser
     # what we want is to render it using the index.html as a template.
     # return send_file(f"{tex_file.name[:-4]}.png", mimetype="image/png")
 
-    url = url_for("static", filename=f"{os.path.basename(tex_file.name)[:-4]}.png")
-    return render_template("index.html", url=url, tikz_code=tikz_code)
+    # url = url_for("static", filename=f"{os.path.basename(tex_file.name)[:-4]}.png")
+    # return render_template("index.html", url=url, tikz_code=tikz_code)
+
+    # instead of using the url_for method, we can use the base64 encoding method
+    # # so we don't have to copy the file to the static folder
+    with open(f"{tex_file.name[:-4]}.png", "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+
+    # remove the temporary files
+    for f in glob.glob(f"{tex_file.name[:-4]}.*"):
+        subprocess.call(["rm", "-rf", f])
+    return render_template("index.html", image_data=encoded_string, tikz_code=tikz_code)
 
 
 if __name__ == "__main__":
