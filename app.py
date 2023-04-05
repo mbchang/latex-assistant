99420d2ecfc21b11835336e0f3df388329c35b8f
Michael Chang <95changmichael13@gmail.com>
2023-04-05 12:26:34 -0700

    feat: take text description and return TikZ and rendered image

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import base64
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import glob
 import tempfile
+import openai
 import os
 import subprocess
 
@@ -13,9 +14,42 @@ def index():
     return render_template("index.html")
 
 
+def generate_prompt(tikz):
+    return f"""I am an expert at TikZ.
+Here is the TikZ code that fits the description: {tikz}.
+\\documentclass[tikz,margin=2mm]{{standalone}}
+\\begin{{document}}
+\\begin{{tikzpicture}}"""
+
+
+def post_process(tikz):
+    latex = f"""\\documentclass[tikz,margin=2mm]{{standalone}}
+
+\\begin{{document}}
+\\begin{{tikzpicture}}
+{tikz}
+\\end{{tikzpicture}}
+\\end{{document}}"""
+    return latex.strip()
+
+
 @app.route("/", methods=["POST"])
 def render_tikz():
-    tikz_code = request.form.get("tikz_code", "")
+    # get tikz description from the form
+    tikz_description = request.form.get("tikz_description", "")
+
+    # ask OpenAI to generate the tikz code
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(tikz_description),
+        max_tokens=1024,
+        n=1,
+        suffix="\\end{tikzpicture}",
+        stop=["\\end{tikzpicture}"],
+        temperature=0.7,
+    )
+    tikz_code = post_process(response.choices[0].text)
+
     with tempfile.NamedTemporaryFile(suffix=".tex") as tex_file:
         print(f"Temporary file location: {tex_file.name}")
         tex_file.write(tikz_code.encode())
@@ -23,8 +57,6 @@ def render_tikz():
         subprocess.call(
             [
                 "pdflatex",
-                # f"-interaction=nonstopmode -output-directory={os.path.dirname(tex_file.name)}",
-                # -interaction=nonstopmode causes it to save the file in current directory
                 f"-output-directory={os.path.dirname(tex_file.name)}",
                 tex_file.name,
             ]
@@ -54,14 +86,6 @@ def render_tikz():
                 f"{tex_file.name[:-4]}.png",
             ]
         )
-        # a hacky method that copies the file in the tmp folder to the static folder
-        # subprocess.call(["cp", f"{tex_file.name[:-4]}.png", "static/"])
-    # this by itself just renders the entire image in the browser
-    # what we want is to render it using the index.html as a template.
-    # return send_file(f"{tex_file.name[:-4]}.png", mimetype="image/png")
-
-    # url = url_for("static", filename=f"{os.path.basename(tex_file.name)[:-4]}.png")
-    # return render_template("index.html", url=url, tikz_code=tikz_code)
 
     # instead of using the url_for method, we can use the base64 encoding method
     # # so we don't have to copy the file to the static folder
@@ -71,7 +95,15 @@ def render_tikz():
     # remove the temporary files
     for f in glob.glob(f"{tex_file.name[:-4]}.*"):
         subprocess.call(["rm", "-rf", f])
-    return render_template("index.html", image_data=encoded_string, tikz_code=tikz_code)
+    return render_template(
+        "index.html",
+        image_data=encoded_string,
+        tikz_code=tikz_code,
+        tikz_description=tikz_description,
+    )
+
+    # also need to handle the case when the button is clicked but no code is entered
+    # as well as the case where the code does not compile
 
 
 if __name__ == "__main__":
